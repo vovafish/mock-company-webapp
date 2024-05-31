5776763c5c3cd0c14f83110385771bc2c373bda1
Vladimirs <vladimirribakov123@mail.ru>
2024-05-31 16:13:59 +0100

    added code to return empty list to fail the test

--- src/main/java/com/mockcompany/webapp/service/SearchService.java
+++ src/main/java/com/mockcompany/webapp/service/SearchService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+
 import java.util.List;
 
 @Service
@@ -21,19 +23,25 @@ public class SearchService {
 
     public Collection<ProductItem> search(String query) {
         /*
-         * This is a simple implementation that loops over all the items and does the filtering in Java.
-         * A better implementation would do most if not all of the filtering in a query and then finalize or
-         * rank the results in Java.  From the SearchControllerSpec, we need to meet the following requirements:
-         *   1. query can be contained within either the name or description of the item
-         *   2. query string is treated as case-insensitive meaning Hi will match hi, hI, Hi, or HI
-         *   3. If the query is wrapped in quotes, only EXACT matches of name/description will be returned
+         * This is a simple implementation that loops over all the items and does the
+         * filtering in Java.
+         * A better implementation would do most if not all of the filtering in a query
+         * and then finalize or
+         * rank the results in Java. From the SearchControllerSpec, we need to meet the
+         * following requirements:
+         * 1. query can be contained within either the name or description of the item
+         * 2. query string is treated as case-insensitive meaning Hi will match hi, hI,
+         * Hi, or HI
+         * 3. If the query is wrapped in quotes, only EXACT matches of name/description
+         * will be returned
          */
         Iterable<ProductItem> allItems = this.productItemRepository.findAll();
         List<ProductItem> itemList = new ArrayList<>();
 
         /*
-         * 1. Check for quotes in the query string. We should use a regex for this but for simplicity
-         *    we will just check and extract using startsWith/endsWith/subString
+         * 1. Check for quotes in the query string. We should use a regex for this but
+         * for simplicity
+         * we will just check and extract using startsWith/endsWith/subString
          */
         boolean exactMatch = false;
         if (query.startsWith("\"") && query.endsWith("\"")) {
@@ -45,7 +53,8 @@ public class SearchService {
             query = query.toLowerCase();
         }
 
-        // For each item... This is written for simplicity to be read/understood not necessarily maintain or extend
+        // For each item... This is written for simplicity to be read/understood not
+        // necessarily maintain or extend
         for (ProductItem item : allItems) {
             boolean nameMatches;
             boolean descMatches;
@@ -56,7 +65,8 @@ public class SearchService {
                 // Check if description is an exact match
                 descMatches = query.equals(item.getDescription());
             } else {
-                // We are doing a contains ignoring case check, normalize everything to lowercase
+                // We are doing a contains ignoring case check, normalize everything to
+                // lowercase
                 // Check if name contains query
                 nameMatches = item.getName().toLowerCase().contains(query);
                 // Check if description contains query
@@ -69,6 +79,6 @@ public class SearchService {
             }
         }
         // Return results
-        return itemList;
+        return Collections.emptyList();
     }
 }
